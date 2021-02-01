71f7b1c31f15c2cd585bbcc1c9ce25776670edc6
Jon Zlotnik <me@jonzlotnik.info>
2021-02-01 17:57:28 -0500

    done parts a-d

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # soen-345
+
+## Assignment 1 Instructions
+
+``` bash
+docker build -t soen345ass1 -f Dockerfile .
+
+docker run -ti soen345ass1
+```
--- ass1-entrypoint.sh
+++ ass1-entrypoint.sh
@@ -1,50 +1,27 @@
 #! /bin/sh -eu
 
-# Copy files from /usr/share/maven/ref into ${MAVEN_CONFIG}
-# So the initial ~/.m2 is set with expected content.
-# Don't override, as this is just a reference setup
+# export USER_HOME_DIR=/root
 
-copy_reference_files() {
-  local log="$MAVEN_CONFIG/copy_reference_file.log"
-  local ref="/usr/share/maven/ref"
+# Assignment 1
 
-  if mkdir -p "${MAVEN_CONFIG}/repository" && touch "${log}" > /dev/null 2>&1 ; then
-      cd "${ref}"
-      local reflink=""
-      if cp --help 2>&1 | grep -q reflink ; then
-          reflink="--reflink=auto"
-      fi
-      if [ -n "$(find "${MAVEN_CONFIG}/repository" -maxdepth 0 -type d -empty 2>/dev/null)" ] ; then
-          # destination is empty...
-          echo "--- Copying all files to ${MAVEN_CONFIG} at $(date)" >> "${log}"
-          cp -rv ${reflink} . "${MAVEN_CONFIG}" >> "${log}"
-      else
-          # destination is non-empty, copy file-by-file
-          echo "--- Copying individual files to ${MAVEN_CONFIG} at $(date)" >> "${log}"
-          find . -type f -exec sh -eu -c '
-              log="${1}"
-              shift
-              reflink="${1}"
-              shift
-              for f in "$@" ; do
-                  if [ ! -e "${MAVEN_CONFIG}/${f}" ] || [ -e "${f}.override" ] ; then
-                      mkdir -p "${MAVEN_CONFIG}/$(dirname "${f}")"
-                      cp -rv ${reflink} "${f}" "${MAVEN_CONFIG}/${f}" >> "${log}"
-                  fi
-              done
-          ' _ "${log}" "${reflink}" {} +
-      fi
-      echo >> "${log}"
-  else
-    echo "Can not write to ${log}. Wrong volume permissions? Carrying on ..."
-  fi
-}
+# Experiments - Part A
 
-owd="$(pwd)"
-copy_reference_files
-unset MAVEN_CONFIG
+cp -R /root/ass1/part_a /root/ass1/out_part_a
 
-cd "${owd}"
-unset owd
+# Experiments - Part B
+
+cp -R /root/ass1/part_b /root/ass1/out_part_b
+
+# Experiments - Part C
+
+cp -R /root/ass1/part_c /root/ass1/out_part_c
+
+# Experiments - Part D
+
+cp -R /root/ass1/part_d /root/ass1/out_part_d
+
+# Experiments - Part E
+
+cp -R /root/ass1/part_e /root/ass1/out_part_e
 
 exec "$@"
